ec7f6cebb5c5eb02d585592c7ee25d7fa902d1e0
Arthur Pedroti Note <arthurpedroti@gmail.com>
2020-03-21 22:19:56 -0300

    nunjucks

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+const server = express();
+const recipes = require("./data");
+
+server.use(express.static("public"));
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+  express: server,
+  autoescape: false,
+  noCache: true
+});
+
+server.get("/", function(req, res) {
+  return res.render("index", { items: recipes });
+});
+
+server.get("/about", function(req, res) {
+  return res.render("about");
+});
+
+server.get("/recipes", function(req, res) {
+  return res.render("recipes", { items: recipes });
+});
+
+server.get("/recipes/:index", function(req, res) {
+  const recipeIndex = req.params.index;
+  if (recipes[recipeIndex] === undefined) {
+    return res.send("Receita não encontrada!");
+  } else {
+    return res.render("recipepage", { item: recipes[recipeIndex] });
+  }
+});
+
+server.listen(5000, function() {
+  console.log("server is running");
+});
